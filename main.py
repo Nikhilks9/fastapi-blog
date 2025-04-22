0a23cfeb125aeba3dbddb62329998acc1b1820e7
Nikhilks9 <mailnikhil.singh2111@gmail.com>
2025-04-22 09:56:53 +0530

    updated return in main file

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
 
 @app.get("/")
 def hello():
-    return {"msg":"Hello FastAPI 🚀"}
+    return {"msg":"Hello FastAPI 🚀 hehe"}
 
